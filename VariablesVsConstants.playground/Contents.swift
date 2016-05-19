fd0855f3c64718d6e2e752ca8a2af17536219530
Diego Freniche Brito <diego@freniche.com>
2016-05-19 20:20:06 +0200

    Initial commit

---
+++ VariablesVsConstants.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: # Variables vs Constants
+
+
+// to create a var use:
+var str = "Hello, playground"
+
+// to create a constant use:
+let thisIsConstant = "Constant"
+
+//: Create a constant containing the number 10
+var myConstant = 10
+
+myConstant = 11
+
+//:
+
