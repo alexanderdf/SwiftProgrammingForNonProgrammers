6b6e4e8fff519139aee8939d85f5cbd448642e2b
Alejandro Valero Pastor <alexandervalero77@gmail.com>
2016-05-19 20:44:18 +0200

    New variable created. Welcome.

--- VariablesVsConstants.playground/Contents.swift
+++ VariablesVsConstants.playground/Contents.swift
@@ -4,13 +4,15 @@
 // to create a var use:
 var str = "Hello, playground"
 
+var str2 = "Hello, Sevilla"
+
 // to create a constant use:
 let thisIsConstant = "Constant"
 
 //: Create a constant containing the number 10
 var myConstant = 10
 
-myConstant = 11
+myConstant = 1
 
 // change
 
